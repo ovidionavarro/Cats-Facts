903d8605e6ad17cee78e3de3cc2445e66ec8e7a9
ovidionavarro <ovidionavarro90@gmail.com>
2024-12-18 07:45:06 -0500

    div useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,21 @@ import './style.css'
 export function App() {
     const RANDOM_FACT = 'https://catfact.ninja/fact'
     // const CAT_ENDPOINT_IMAGE_URL= `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
-    const [fact, setFact] = useState()
+    const [fact, setFact] = useState(null)
     const [ImageURL, setImageURL] = useState(null)
     useEffect(() => {
         fetch(RANDOM_FACT)
-            .then(res => res.json())
+            .then(res => {
+                if(!res.ok) throw new Error('error fetching fact')
+                return res.json()
+            })
             .then(data => {
 
 
                 const { fact } = data
                 setFact(fact)
-
-                const first_word = fact.split(' ',3).join('%20%20')
-                const url_IMG=`https://cataas.com/cat/says/${first_word}?font=Impact&fontSize=30&fontColor=rojo&fontBackground=none&position=center&json=false`
-                setImageURL(url_IMG)
-
             })
-
-
-
+            
 
         // async function GetRandomFact() {
         //     const res=await fetch(RANDOM_FACT)
@@ -31,6 +27,14 @@ export function App() {
         // GetRandomFact()
 
     }, [])
+    useEffect(() => {
+        if(!fact) return
+        const first_word = fact.split(' ',3).join('%20%20')
+        const url_IMG=`https://cataas.com/cat/says/${first_word}?font=Impact&fontSize=30&fontColor=rojo&fontBackground=none&position=center&json=false`
+        setImageURL(url_IMG)
+        
+    }, [fact])
+    
 
     return (
         <main>
